1da0e0a7812fe40fb43fbe5f19b688bcc0a42ff7
juan felipe nauffal <jfnauffal@correo.usbcali.edu.co>
2026-05-11 10:17:11 -0500

    arreglo sobre lo del usuario y la contraseña

--- rpg-platform-/RPG/settings.py
+++ rpg-platform-/RPG/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'RPG.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'rpg_platform',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 

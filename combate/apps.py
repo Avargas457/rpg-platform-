a741aa9a97db9705772aadcfca8e86213bdcf600
Andres Vargas <avargasm2@correo.usbcali.edu.co>
2026-05-11 11:50:16 -0500

    Empty Samuel-Ceballos branch

--- combate/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class CombateConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'combate'
-    verbose_name = 'Combates'
--- tirada/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class TiradaConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'tirada'
-    verbose_name = 'Tiradas de Dado'
